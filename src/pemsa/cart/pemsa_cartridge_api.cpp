d6a59aa9e832f78e3e8c3a4227daa75206f27b40
eugodr <egordorichev@gmail.com>
2021-10-20 16:11:59 +0300

    Splore is working pretty well

--- src/pemsa/cart/pemsa_cartridge_api.cpp
+++ src/pemsa/cart/pemsa_cartridge_api.cpp
@@ -1,6 +1,11 @@
 #include "pemsa/pemsa_emulator.hpp"
 
 #include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+
 namespace fs = std::filesystem;
 
 static PemsaEmulator* emulator;
@@ -59,17 +64,96 @@ static int list_carts(lua_State* state) {
 	int i = 0;
 
 	for (const auto & entry : fs::directory_iterator("./")) {
+		if (entry.is_directory()) {
+			continue;
+		}
+
 		lua_newtable(state);
 
-			lua_pushstring(state, entry.path().stem().c_str());
-			lua_rawseti(state, -2, 1);
-			lua_pushstring(state, "egor");
-			lua_rawseti(state, -2, 2);
+		lua_pushstring(state, entry.path().stem().c_str());
+		lua_rawseti(state, -2, 1);
+		lua_pushstring(state, "egor");
+		lua_rawseti(state, -2, 2);
 
-			lua_rawseti(state, -2, i++);
+		lua_rawseti(state, -2, i++);
 	}
 
 	return 1;
+}
+
+static inline void ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+		return !std::isspace(ch);
+	}));
+}
+
+static inline void rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+		return !std::isspace(ch);
+	}).base(), s.end());
+}
+
+static int read_cdata(lua_State* state) {
+	if (lua_gettop(state) == 0) {
+		return 0;
+	}
+
+	const char* path = pemsa_to_string(state, 1);
+	std::ifstream file(path);
+
+	if (file.bad() || !file.is_open()) {
+		file.close();
+		file.open(path + std::string(".p8"));
+
+		if (file.bad() || !file.is_open()) {
+			std::cerr << "Failed to open the file\n";
+			return 0;
+		}
+	}
+
+	std::string line;
+
+	for (int i = 0; i < 3; i++) {
+		std::getline(file, line); // Skip headers, version & __lua__
+	}
+
+	if (line != "__lua__") {
+		std::cerr << "No __lua__ mark found as expected\n";
+		return 0;
+	}
+
+	std::string name;
+	std::string author;
+
+	std::getline(file, name);
+	std::getline(file, author);
+
+	ltrim(name);
+	ltrim(author);
+	rtrim(name);
+	rtrim(author);
+
+	if (name[0] == '-' && name[1] == '-') {
+		name = name.substr(2, name.length());
+	} else {
+		name = std::string("unknown");
+	}
+
+	if (author[0] == '-' && author[1] == '-') {
+		author = author.substr(2, author.length());
+	} else {
+		author = std::string("unknown");
+	}
+
+	ltrim(name);
+	ltrim(author);
+	rtrim(name);
+	rtrim(author);
+
+	lua_pushstring(state, name.c_str());
+	lua_pushstring(state, author.c_str());
+
+	return 2;
 }
 
 void pemsa_open_cartridge_api(PemsaEmulator* machine, lua_State* state) {
@@ -82,4 +166,5 @@ void pemsa_open_cartridge_api(PemsaEmulator* machine, lua_State* state) {
 	lua_register(state, "__reset", reset);
 	lua_register(state, "__load", load);
 	lua_register(state, "__list_carts", list_carts);
+	lua_register(state, "__read_cdata", read_cdata);
 }
